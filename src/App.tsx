98da3d5cf1151ed57ff7386f8653a4dd14c7006a
Bolat Abdulaziz <Bola_cg56@student.itstep.kz>
2024-01-27 23:19:50 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import MobileChat from "./pages/MobileChat/MobileChat";
-import MainApp from "./pages/MainApp/MainApp";
+import MainApp from "./pages/MainApp";
+import BuyPolis from "./pages/BuyPolis";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route path="/" element={<MobileChat/>}></Route>
               <Route path="/main-app" element={<MainApp/>}></Route>
+              <Route path="/buy-polis" element={<BuyPolis/>}></Route>
           </Routes>
       </Router>
   );
--- src/pages/MainApp.tsx
+++ src/pages/MainApp.tsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import login from "../../images/login.svg"
-import language from "../../images/language.svg"
-import notification from "../../images/notification.svg"
-import buyPolis from "../../images/buy-polis.svg"
-import pay from "../../images/pay.svg"
-import insuranceCase from "../../images/insurance-case.svg"
-import history from "../../images/history.svg"
-import myPolis from "../../images/my-polis.svg"
-import request from "../../images/request.svg"
 
 const MainApp = () => {
     return (
@@ -15,7 +6,7 @@ const MainApp = () => {
             <header className="w-[100%] flex justify-between py-2">
                 <div className="flex items-center gap-3 pl-2">
                     <div className="p-2 flex justify-center items-center border border-[#F0F0F0] bg-[#fff] rounded-lg">
-                        <img className="w-[28px] h-[28px]" src={login}/>
+                        <img className="w-[28px] h-[28px]" src="/icons/login.svg"/>
                     </div>
                     <div className="flex flex-col text=[28px] font-bold">
                         <a href="#">Вход</a>
@@ -24,36 +15,36 @@ const MainApp = () => {
                 </div>
                 <div className="flex gap-[30px]">
                     <div className="flex justify-center items-center gap-[10px]">
-                        <img src={language} alt={``}/>
+                        <img src="/icons/language.svg" alt={``}/>
                         <p>RU</p>
                     </div>
-                    <img src={notification} alt={``}/>
+                    <img src="/icons/notification.svg" alt={``}/>
                 </div>
             </header>
 
             <div className="w-[95%] my-4 py-4 flex flex-wrap items-start bg-[#fff] rounded-xl">
                 <div className="w-1/3 py-2 flex flex-col justify-center items-center">
-                    <img src={buyPolis}/>
+                    <img src="/icons/buy-polis.svg"/>
                     <p className="w-1/2 text-[12px] text-center">Купить полисы</p>
                 </div>
                 <div className="w-1/3 py-2 flex flex-col justify-center items-center">
-                    <img src={pay}/>
+                    <img src="/icons/pay.svg"/>
                     <p className="w-1/2 text-[12px] text-center">Оплатить</p>
                 </div>
                 <div className="w-1/3 py-2 flex flex-col justify-center items-center">
-                    <img src={insuranceCase}/>
+                    <img src="/icons/insurance-case.svg"/>
                     <p className="w-1/2 text-[12px] text-center">Страховой случай</p>
                 </div>
                 <div className="w-1/3 py-2 flex flex-col justify-center items-center">
-                    <img src={history}/>
+                    <img src="/icons/history.svg"/>
                     <p className="w-1/2 text-[12px] text-center">Операция</p>
                 </div>
                 <div className="w-1/3 py-2 flex flex-col justify-center items-center">
-                    <img src={myPolis}/>
+                    <img src="/icons/my-polis.svg"/>
                     <p className="w-1/2 text-[12px] text-center">Мои полисы</p>
                 </div>
                 <div className="w-1/3 py-2 flex flex-col justify-center items-center">
-                    <img src={request}/>
+                    <img src="/icons/request.svg"/>
                     <p className="w-1/2 text-[12px] text-center">Заявки</p>
                 </div>
             </div>
