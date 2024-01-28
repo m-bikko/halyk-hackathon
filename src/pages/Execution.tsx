d2319d8b302e68c6b1763d7ae5b5454840751301
Beimbet <90394622+m-bikko@users.noreply.github.com>
2024-01-28 13:32:23 +0600

    Update Execution.tsx

--- src/pages/Execution.tsx
+++ src/pages/Execution.tsx
@@ -1,21 +1,101 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import {Bottom} from "../components/Bottom";
 
-
 const Execution = () => {
-    const [price, setPrice] = useState(50);
+    const [price, setPrice] = useState(5000);
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+    const [territory, setTerritory] = useState<string | null>(null);
+    const [insuranceTerm, setInsuranceTerm] = useState<number | null>(null);
+    const [paymentFrequency, setPaymentFrequency] = useState<string | null>(null);
 
     const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPrice(Number(event.target.value));
     };
 
-
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-
     const handleDateChange = (date: Date | null) => {
         setSelectedDate(date);
+    };
+
+    const handleTerritoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setTerritory(event.target.value);
+    };
+
+    const handleInsuranceTermChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setInsuranceTerm(Number(event.target.value));
+    };
+
+    const handlePaymentFrequencyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setPaymentFrequency(event.target.value);
+    };
+
+    const hasCriminalRecord = false, hasDisability = false, hasDrugRecord = false, isHivPositive = true
+
+
+    useEffect(() => {
+        let insuranceSum = 0;
+
+        if (insuranceTerm && paymentFrequency && territory) {
+            const ageCoefficient = calculateAgeCoefficient(); // Расчет коэффициента возраста
+            const criminalRecordCoefficient = !hasCriminalRecord ? 1.2 : 0.2; // Коэффициент для наличия/отсутствия судимости
+            const drugRecordCoefficient = hasDrugRecord ? 1.2 : 0.2; // Коэффициент для наличия/отсутствия записи в наркодиспансере
+            const hivStatusCoefficient = isHivPositive ? 1.2 : 0.2; // Коэффициент для наличия/отсутствия ВИЧ
+            const disabilityCoefficient = hasDisability ? 1.2 : 1; // Коэффициент для наличия/отсутствия инвалидности
+
+            let baseInsuranceSum = 0;
+            switch (paymentFrequency) {
+                case 'Ежемесячная':
+                    baseInsuranceSum = price * insuranceTerm;
+                    break;
+                case 'Квартальная':
+                    baseInsuranceSum = price * insuranceTerm * 3;
+                    break;
+                case 'Полугодовая':
+                    baseInsuranceSum = price * insuranceTerm * 6;
+                    break;
+                case 'Годовая':
+                    baseInsuranceSum = price * insuranceTerm * 12;
+                    break;
+                case 'Единовременно':
+                    baseInsuranceSum = price * insuranceTerm;
+                    break;
+                default:
+                    baseInsuranceSum = 0;
+            }
+
+            insuranceSum = baseInsuranceSum * ageCoefficient * criminalRecordCoefficient * drugRecordCoefficient * hivStatusCoefficient * disabilityCoefficient;
+        }
+
+        const deathAnyReason = insuranceSum;
+        const deathAccident = insuranceSum;
+        const survival = insuranceSum;
+
+        document.getElementById('deathAnyReasonValue')!.innerText = deathAnyReason.toLocaleString('en-US') + ' тг';
+        document.getElementById('deathAccidentValue')!.innerText = deathAccident.toLocaleString('en-US') + ' тг';
+        document.getElementById('survivalValue')!.innerText = survival.toLocaleString('en-US') + ' тг';
+    }, [price, selectedDate, insuranceTerm, paymentFrequency, territory, hasCriminalRecord, hasDrugRecord, isHivPositive, hasDisability]);
+
+
+    const calculateAgeCoefficient = () => {
+        const age = calculateAge();
+        if (age < 30) {
+            return 1.2;
+        } else if (age >= 30 && age < 50) {
+            return 1.1;
+        } else {
+            return 1;
+        }
+    };
+
+    const calculateAge = () => {
+        const birthDate = selectedDate;
+        if (birthDate) {
+            const currentYear = new Date().getFullYear();
+            const birthYear = birthDate.getFullYear();
+            return currentYear - birthYear;
+        }
+        return 0;
     };
 
     return (
@@ -38,22 +118,31 @@ const Execution = () => {
                     className="w-full h-[3px]"
                 />
             </div>
-            <select className="w-[90%] h-[50px]  pl-3 my-3 border rounded-lg border-[#D7D7D7]">
+            <select
+                className="w-[90%] h-[50px] pl-3 my-3 border rounded-lg border-[#D7D7D7]"
+                onChange={handleTerritoryChange}
+            >
                 <option hidden selected>Территория страхования</option>
-                <option>Almaty</option>
-                <option>Atyrau</option>
-                <option>Shymkent</option>
-                <option>Astana</option>
+                <option value="Almaty">Almaty</option>
+                <option value="Atyrau">Atyrau</option>
+                <option value="Shymkent">Shymkent</option>
+                <option value="Astana">Astana</option>
             </select>
-            <select className="w-[90%] h-[50px] pl-3 my-3 border border-[#D7D7D7] rounded-lg">
+            <select
+                className="w-[90%] h-[50px] pl-3 my-3 border border-[#D7D7D7] rounded-lg"
+                onChange={handleInsuranceTermChange}
+            >
                 <option hidden selected>Срок страхования</option>
-                <option>1 месяц</option>
-                <option>3 месяц</option>
-                <option>6 месяц</option>
-                <option>1 год</option>
-                <option>2 год</option>
+                <option value={1}>1 месяц</option>
+                <option value={3}>3 месяц</option>
+                <option value={6}>6 месяц</option>
+                <option value={12}>1 год</option>
+                <option value={24}>2 года</option>
             </select>
-            <select className="w-[90%] h-[50px] pl-3 my-3 border border-[#D7D7D7] rounded-lg">
+            <select
+                className="w-[90%] h-[50px] pl-3 my-3 border border-[#D7D7D7] rounded-lg"
+                onChange={handlePaymentFrequencyChange}
+            >
                 <option hidden selected>Периодичность оплаты</option>
                 <option>Ежемесячная</option>
                 <option>Квартальная</option>
@@ -61,7 +150,6 @@ const Execution = () => {
                 <option>Годовая</option>
                 <option>Единовременно</option>
             </select>
-
 
             <div className="w-[90%] h-[50px] my-3 flex items-center border border-[#D7D7D7]  rounded-lg relative">
                 <DatePicker
@@ -91,15 +179,15 @@ const Execution = () => {
             <table className="mr-2 ml-2">
                 <tr>
                     <td className="w-1/2 border p-4">Смерть Застрахованного по любой причине</td>
-                    <td className="w-1/2 border p-4">0 тг</td>
+                    <td className="w-1/2 border p-4" id={"deathAnyReasonValue"}>0тг</td>
                 </tr>
                 <tr>
                     <td className="w-1/2 border p-4">Смерть Застрахованного по причине несчастного случая</td>
-                    <td className="w-1/2 border p-4">0 тг</td>
+                    <td className="w-1/2 border p-4" id={"deathAccidentValue"}>0 тг</td>
                 </tr>
                 <tr>
                     <td className="w-1/2 border p-4">Дожитие Застрахованного до конца срока страхования</td>
-                    <td className="w-1/2 border p-4">0 тг</td>
+                    <td className="w-1/2 border p-4" id={"survivalValue"}>0 тг</td>
                 </tr>
             </table>
 
